7db32e024081858e8877e2919f0a8572c81dd7cd
Swag-blips <coderblip@gmail.com>
2024-07-24 15:28:14 +0100

    javascript complete

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 "use strict";
+
+let emailInput = document.querySelector(".emailInput");
+let submitButton = document.querySelector(".submit");
+let mainContent = document.querySelector(".main-content");
+let imgContainer = document.querySelector(".img-container");
+let thankYouPage = document.querySelector(".thank-you");
+let errorMessage = document.getElementById("error-message");
+let confirmationMessage = document.getElementById("confirmation-message");
+let emailRegex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+let dismissMessage = document.querySelector(".dismiss-message");
+
+document.addEventListener("DOMContentLoaded", () => {
+  let subscribe = (e) => {
+    e.preventDefault();
+
+    if (validateInput()) {
+      mainContent.style.display = "none";
+      imgContainer.style.display = "none";
+      thankYouPage.style.display = "flex";
+      generateSuccessMessage(emailInput);
+    } else {
+      emailInput.style.border = "1px solid rgba(255,97,85)";
+      emailInput.style.background = "rgba(255,97,85,15%)";
+      emailInput.style.color = "rgba(255,97,85)";
+      errorMessage.style.display = "flex";
+    }
+  };
+
+  submitButton.addEventListener("click", subscribe);
+
+  const validateInput = () => {
+    let isValid = false;
+
+    if (!emailInput.value.trim() === "") {
+      isValid = true;
+    } else if (emailRegex.test(emailInput.value.trim())) {
+      isValid = true;
+    }
+
+    return isValid;
+  };
+
+  const generateSuccessMessage = (email) => {
+    let markup = `
+       A confirmation email has been sent to
+            <span>${email.value}.</span> Please open it and click the
+            button inside to confirm your subscription
+    `;
+
+    confirmationMessage.innerHTML = markup;
+  };
+});
